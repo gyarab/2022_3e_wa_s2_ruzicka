52404f1cc7338f75f3d872305ef11b6855b3bdd8
Firu <filip.ruzicka@student.gyarab.cz>
2023-01-29 20:40:14 +0100

    konvertor

--- python_intro/kurz.py
+++ python_intro/kurz.py
@@ -1,9 +1,20 @@
 import httpx
+from colorama import Fore
 
-url = "https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt"
+url = 'https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt'
 
 resp = httpx.get(url)
 
-for i in resp.text.split("\n"):
-    if "EUR" in i:
-        print("Kurz eura je: " + i.split('|')[-1])
+mena = input(Fore.MAGENTA + 'Vyberte měnu ze které chcete konvertovat: ').upper()
+
+kurz = False
+for i in resp.text.split('\n'):
+    if mena in i:
+        kurz = float(i.split('|')[-1].replace(',', '.')) / int(i.split('|')[2])
+        break
+else:
+    print(Fore.RED + f'K měně {mena} nemáme informace.')
+
+if kurz:
+    obnos = int(input(Fore.MAGENTA + f'Obnos {mena}: '))
+    print(Fore.LIGHTBLUE_EX + f'\n{obnos} {mena} => {round(obnos * kurz, 3)} CZK')
